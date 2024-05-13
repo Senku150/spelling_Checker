dad62d1e87293036c81118c9d6c84fbf22f43af4
Momin-Ahmed-Mebarez <momenmomen35@gmail.com>
2024-05-13 20:24:43 +0300

    Clien

--- client.py
+++ client.py
@@ -1,6 +1,11 @@
 import pyttsx3
 import requests
 
+engine = pyttsx3.init()
 
 
-
+response = requests.get("http://192.168.1.10:5000")
+word = response.text
+print(word)
+engine.say(word)
+engine.runAndWait()
