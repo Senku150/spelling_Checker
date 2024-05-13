24113aec0a7feb82ef61ede87f659ee33a0583cb
Momin-Ahmed-Mebarez <momenmomen35@gmail.com>
2024-05-13 20:16:04 +0300

    Server

--- server.py
+++ server.py
@@ -1,9 +1,8 @@
-import pyttsx3
 from flask import Flask
-import math
 import random
-import json
 import socket
+
+
 
 def read_specific_line(file_path, line_number):
   with open(file_path, "r") as f:
@@ -18,58 +17,21 @@ def read_specific_line(file_path, line_number):
 
     # Read and return the specific line
     return f.readline().strip()  # Read and remove trailing newline
-random.seed()
-randomNum=random.randint(1,89058)
-line_content = read_specific_line("anagram_dictionary.txt", randomNum)
-
-print(line_content)
-pyttsx3.speak(line_content)
 
 
 
+app = Flask(__name__)
 
-"""app = Flask(__name__)
+@app.route("/",methods=["GET"])
+def index():
+  random.seed()
+  randomNum=random.randint(1,89058)
+  line_content = read_specific_line("anagram_dictionary.txt", randomNum)
+  return line_content
 
-hidden = None
-
-tries = 0
-
-
-#Creating the game
-def generate(low, high, ip):
-    random.seed()
-    global hidden
-    global tries
-
-    #Swapping the values if the user entered them in the wrong order
-    temp = 0
-    if(low > high):
-        temp = low
-        low = high
-        high = temp
-
-    
-    tries = round(math.log(high-low+1,2)) #The equation that calculates the minimum tries is not mine
-    hidden = random.randint(low, high)#Chosing the random number
-    
+if __name__ == "__main__":
+    ip = socket.gethostbyname(socket.gethostname())
     app.run(debug=False, host=ip, port=5000)
 
 
 
-
-@app.route("/", methods=["GET"])
-def index():
-    #Sending the hidden number and number of tries to any user that connects to the server
-    return json.dumps({"hidden" : hidden, "tries" : tries})
-    
-
-
-
-
-
-if __name__ == "__main__":
-    ip = socket.gethostbyname(socket.gethostname())
-    low = int(input("Enter the lowest number: "))
-    high = int(input("Enter the highest number: "))
-    print(f"[**] Use {ip} as the ip for clients [**] \n Ignore everything under this message\n")
-    generate(low, high, ip)"""
